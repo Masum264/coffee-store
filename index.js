c6cb5ef92bd14d78a37e5e1ee485683180880c0b
Masum264 <masumh246@gmail.com>
2025-05-22 22:51:09 +0600

    Third commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 3000;
@@ -36,10 +36,26 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/coffees/:id', async(req,res) =>{
+
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await coffeesCollection.findOne(query);
+      res.send(result);
+    })
+
     app.post('/coffees', async(req, res)=>{
       const newCoffee = req.body;
       console.log(newCoffee);
       const result = await coffeesCollection.insertOne(newCoffee);
+      res.send(result);
+    })
+
+    app.delete('/coffees/:id', async(req,res) =>{
+
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await coffeesCollection.deleteOne(query);
       res.send(result);
     })
     // Send a ping to confirm a successful connection
